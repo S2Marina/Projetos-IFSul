0bbf19f011a1b4e100cc630008cbdd77b83baead
S2Marina <marina_silva98@hotmail.com>
2015-04-08 11:05:21 -0300

    Git Hub
    08/04/2015

---
+++ jogo/src/dao/JogadorDAO.java
@@ -0,0 +1,52 @@
+package dao;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.Jogador;
+        
+public class JogadorDAO {
+    public boolean inserir(Jogador jogador){
+        Boolean retorno;
+        String sql = "INSERT INTO jogador(login,senha,email) VALUES(?,?,?)"; //monta o sql de insert na tabela
+        PreparedStatement pst = Conexao.getPreparedStatement(sql);
+        
+        try {
+            pst.setString(1,jogador.getLogin());
+            pst.setString(2,jogador.getSenha());//insere os parametros
+            pst.setString(3,jogador.getEmail());
+            
+            pst.executeUpdate();//executa o sql no banco
+            retorno = true;         
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            retorno = false;
+        }
+        
+        return retorno;
+    }
+    
+    public List<Jogador> listar(){
+        List<Jogador> lista = new ArrayList<Jogador>(); //cria a lista de jogadores
+        String sql = "SELECT * FROM jogador";
+        PreparedStatement pst = Conexao.getPreparedStatement(sql);
+
+        try {
+            ResultSet res = pst.executeQuery();//executo o sql e jogo num ResultSet
+            while(res.next()){ //enquanto tiver registro eu vou relacionar com minha classe Jogador e adicionar na lista
+                Jogador j = new Jogador();
+                j.setLogin(res.getString("login"));
+                j.setSenha(res.getString("senha"));
+                j.setEmail(res.getString("email"));
+                lista.add(j);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(JogadorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       return lista;
+    }
+}
---
+++ jogo/src/dao/PerguntaDAO.java
@@ -0,0 +1,63 @@
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.Pergunta;
+
+public class PerguntaDAO {
+    public boolean inserir(Pergunta pergunta){
+        Boolean retorno;
+        String sql = "INSERT INTO pergunta(enunciado, a, b, c, d, certa, nivel) VALUES(?,?,?,?,?,?,?)"; 
+        PreparedStatement pst = Conexao.getPreparedStatement(sql);
+        
+        try {
+            pst.setString(1, pergunta.getEnunciado());
+            pst.setString(2, pergunta.getA());
+            pst.setString(3, pergunta.getB());
+            pst.setString(4, pergunta.getC());
+            pst.setString(5, pergunta.getD());
+            pst.setString(6, pergunta.getCerta());
+            pst.setInt(7, pergunta.getNivel());
+            
+ .           pst.executeUpdate();//executa o sql no banco
+            retorno = true;         
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            retorno = false;
+        }
+        
+        return retorno;
+    }
+    
+    public List<Pergunta> listar(){
+        List<Pergunta> lista = new ArrayList<Pergunta>(); 
+        String sql = "SELECT * FROM jogador";
+        PreparedStatement pst = Conexao.getPreparedStatement(sql);
+
+        try {
+            ResultSet res = pst.executeQuery();
+            while(res.next()){ 
+                Pergunta p = new Pergunta();
+                p.setId(res.getInt("id"));
+                p.setEnunciado(res.getString("enunciado"));
+                p.setA(res.getString("a"));
+                p.setB(res.getString("b"));
+                p.setC(res.getString("c"));
+                p.setD(res.getString("d"));
+                p.setCerta(res.getString("certa"));                
+                p.setNivel(res.getInt("nivel"));
+                lista.add(p);
+            }
+                
+        } catch (SQLException ex) {
+            Logger.getLogger(PerguntaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       return lista;
+    }
+}
